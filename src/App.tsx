d0164d6babff2ae2a5a414cde6ca24937507831b
Lucas <lsdeabre@gmail.com>
2024-07-19 02:26:20 -0300

    Second approuch

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,53 @@
 import Placeholder from "./components/Placeholder"
-import PlayerX from './components/PlayerX';
-import PlayerO from './components/PlayerO';
-
 import './styles/game.css'
 import { ReactElement, useEffect, useState } from "react";
 
 type GameStates = {
-  [key: string]: string|undefined;
+  [key: string]: boolean;
 }
 
-//TODO: Idenficiar se houve um ganhador a cada mudança de estado de um dos componentes
+const winingTable: number[][] = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 function App() {
-  let [count, setCount] = useState(0)
-  let [currentPlayer, setCurrentPlayer] = useState<string>()
-  const [gameState, setGameState] = useState<GameStates>({})
-
-  let winingTable: number[][] = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
   const board: ReactElement[] = []
-  let playerComponent: ReactElement|undefined = undefined
-
-  const checkWinner = (gameState: GameStates): boolean => {
-    let winnerPosition: string|undefined = ""
-    let winnerPosition1: string|undefined = ""
-    let winnerPosition2: string|undefined = ""
-    let winner:boolean = false
-    for (let chanceWinning of winingTable) {
-      winnerPosition = gameState[chanceWinning[0]]
-      winnerPosition1 = gameState[chanceWinning[1]]
-      winnerPosition2 = gameState[chanceWinning[2]]
-
-      if (winnerPosition && winnerPosition1 && winnerPosition2) {
-        if(winnerPosition === winnerPosition1 && winnerPosition === winnerPosition2 && winnerPosition1 === winnerPosition2){
-          winner = true
-        }
-      }
-    }
-    return winner
-  }
-
-  const onChangePlayer = (id: string): ReactElement => {
-    playerComponent = currentPlayer === "X" ? <PlayerO /> : <PlayerX />
-    setCurrentPlayer(currentPlayer = count % 2 === 0 ? "X" : "O")
-    setCount(count + 1)
-
-    setGameState(prevState => (
-      {
-        ...prevState,
-        [id]: currentPlayer,
-      })
-    )
-
-    return playerComponent
-  }
-
-  const isWin = (): boolean => {
-    return checkWinner(gameState)
-  }
+  let [boardMapping, setBoardMapping]= useState<GameStates>({})
+  let [turn, setTurn] = useState<boolean>(false)
   
+  function playerClicked(id: string) {
+    setTurn(prev => turn = !prev)
+    setBoardMapping(prev => boardMapping = {...prev, [id]: turn})
+  }
 
-  useEffect(() => {
-    console.log(gameState);
+  function isWin() {
+    winingTable.forEach((victoryPosition) => {
+      const playerPosition = boardMapping[victoryPosition[0]]
+      const playerPosition1 = boardMapping[victoryPosition[1]]
+      const playerPosition2 = boardMapping[victoryPosition[2]]
+
+      if (playerPosition != undefined && playerPosition1 != undefined && playerPosition2 != undefined){
+        const isWinner = playerPosition === playerPosition1 && playerPosition === playerPosition2 && playerPosition1 === playerPosition2
+
+        if (isWinner) {
+          turn ? alert("O Wins!"): alert("X Wins!");
+        }
+        //TODO remover verificação de dentro do loop e passar para fora a responsabilidade de retornar o vencendor
+        //TODO inserir casos de empate também
+        //TODO criar um placar e botao para reinicar placar e o jogo
+      }
+    })
+  }
     
-    if (isWin()) {
-      alert(currentPlayer + " Wins!")
-      window.location.reload()  
-    }
-  }, [currentPlayer, onChangePlayer ]);
-
   for (let index = 0; index < 9; index++) {
-    board.push(<Placeholder checkWin={isWin} changePlayer={onChangePlayer} id={index.toString()}/>)    
+    board.push(<Placeholder turn={turn} playerClicked={playerClicked} id={index.toString()} isWin={isWin}/>)    
   }
 
   return (
--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -1,33 +1,42 @@
 import { ReactElement, useEffect, useState } from 'react';
-import '../styles/placeholder.css'
+import '../styles/placeholder.css';
+import PlayerX from './PlayerX';
+import PlayerO from './PlayerO';
+
 
 type Props = {
     id: string;
-    changePlayer: Function;
-    checkWin: Function
+    playerClicked: Function;
+    turn: boolean;
+    isWin: Function;
 }
 
-function Placeholder(props: Props) {
-    let { changePlayer, id, checkWin } = {...props}
-    let [clicked, setClicked] = useState(false)
-    let [player, setPlayer] = useState<ReactElement>()
+function Placeholder(props:Props) {
+    const { id, playerClicked, turn, isWin } = {...props}
+    let [hasChange, setHasChange] = useState<boolean>(false)
+    let [playerX, setPlayerX] = useState<boolean>(false)
+    let [playerO, setPlayerO] = useState<boolean>(false)
 
-    useEffect(() => {
-        if (clicked){
-            checkWin()
-        }  
-    }, [player, changePlayer])
-    
-    const clickedEvent = () => {
-        if (!clicked){
-            setClicked(clicked = true)
-            setPlayer(changePlayer(id))
+    function changeTurn() {
+        playerClicked(id)
+    }
+
+    function showElement() {
+        if (!hasChange) {      
+            turn ? setPlayerX(true) : setPlayerO(true)
+            setHasChange(true)
+            changeTurn()
         }
     }
 
+    useEffect(() => {
+        isWin()
+    }, [playerO, playerX])
+
     return (
-        <div id={id} className="placeholder" onClick={() => clickedEvent()}>
-            {clicked && player}
+        <div id={id} className="placeholder" onClick={() => showElement()}>
+            {playerX && <PlayerX />}
+            {playerO && <PlayerO />}
         </div>
     );
 }
